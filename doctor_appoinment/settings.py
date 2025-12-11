7e0ebb4f82812dec7476fc809457251fc650d93d
Lalit0101 <lalitkp0101@gmail.com>
2025-12-11 11:19:25 +0530

    new update with files

--- doctor_appoinment/settings.py
+++ doctor_appoinment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x30)z^9!$0^3)cbo#^kf(v&jy_b@#&qdzwtt90y6swa@@xhuht'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
