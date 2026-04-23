d1efafef6a156c8ecfa4e01a683edbfef3037061
Lalit0101 <lalitkp0101@gmail.com>
2026-04-23 16:32:53 +0530

    okk

--- appointment/admin.py
+++ appointment/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Category, Doctor, Appointment, Patients, Contact, Billing, Comment
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Doctor)
-admin.site.register(Appointment)
-admin.site.register(Patients)
-admin.site.register(Contact)
-admin.site.register(Billing)
-admin.site.register(Comment)
+# admin.site.register(Category)
+# admin.site.register(Doctor)
+# admin.site.register(Appointment)
+# admin.site.register(Patients)
+# admin.site.register(Contact)
+# admin.site.register(Billing)
+# admin.site.register(Comment)
